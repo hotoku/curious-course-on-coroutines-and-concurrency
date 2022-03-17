93be977dd07cc21ff5ba98091b3a00fdf723ebdf
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-03-18 06:59:25 +0900

    p.84

---
+++ cothread.py
@@ -0,0 +1,24 @@
+from coroutine import coroutine
+from queue import Queue
+from threading import Thread
+
+
+@coroutine
+def threaded(target):
+    messages = Queue()
+
+    def run_target():
+        while True:
+            item = messages.get()
+            if item is GeneratorExit:
+                target.close()
+                return
+            else:
+                target.send(item)
+    Thread(target=run_target).start()
+    try:
+        while True:
+            item = (yield)
+            messages.put(item)
+    except GeneratorExit:
+        messages.put(GeneratorExit)
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from xml.sax import parse
+
 from buses import buses_to_dicts, filter_on_filed, location
-from coexpat import expat_parse
+from cosax import EventHandler
+from cothread import threaded
 
 
-expat_parse(
-    open("bus.xml", "rb"),
-    buses_to_dicts(
-        filter_on_filed(
-            "id", "7574",
-            location()
+parse(
+    "bus.xml",
+    EventHandler(
+        buses_to_dicts(
+            threaded(
+                filter_on_filed(
+                    "id", "7574",
+                    location()
+                )
+            )
         )
     )
 )
