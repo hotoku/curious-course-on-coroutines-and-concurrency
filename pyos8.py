2300640ee60ea7389ed14fcfddadd229e340fa5f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-03-30 20:01:31 +0900

    p.181

---
+++ pyos8.py
@@ -0,0 +1,178 @@
+from __future__ import annotations
+
+import select
+from queue import Queue
+from typing import Any
+import types
+
+
+class Task:
+    taskid = 0
+    sendval: Any
+
+    def __init__(self, target):
+        Task.taskid += 1
+        self.tid = Task.taskid
+        self.target = target
+        self.sendval = None
+        self.stack = []
+
+    def run(self):
+        while True:
+            try:
+                result = self.target.send(self.sendval)
+                if isinstance(result, SystemCall):
+                    return result
+                if isinstance(result, types.GeneratorType):
+                    self.stack.append(self.target)
+                    self.sendval = None
+                    self.target = result
+                else:
+                    if not self.stack:
+                        return
+                    self.sendval = result
+                    self.target = self.stack.pop()
+            except StopIteration:
+                if not self.stack:
+                    raise
+                self.sendval = None
+                self.target = self.stack.pop()
+
+
+class SystemCall:
+    task: Task
+    sched: Scheduler
+
+    def handle(self):
+        pass
+
+
+class GetTid(SystemCall):
+    def handle(self):
+        self.task.sendval = self.task.tid
+        self.sched.schedule(self.task)
+
+
+class NewTask(SystemCall):
+    def __init__(self, target):
+        self.target = target
+
+    def handle(self):
+        tid = self.sched.new(self.target)
+        self.task.sendval = tid
+        self.sched.schedule(self.task)
+
+
+class KillTask(SystemCall):
+    def __init__(self, tid):
+        self.tid = tid
+
+    def handle(self):
+        task = self.sched.taskmap.get(self.tid, None)
+        if task:
+            task.target.close()
+            self.task.sendval = True
+        else:
+            self.task.sendval = False
+        self.sched.schedule(self.task)
+
+
+class WaitTask(SystemCall):
+    def __init__(self, tid):
+        self.tid = tid
+
+    def handle(self):
+        result = self.sched.waitforexit(self.task, self.tid)
+        self.task.sendval = result
+        if not result:
+            self.sched.schedule(self.task)
+
+
+class ReadWait(SystemCall):
+    def __init__(self, f) -> None:
+        self.f = f
+
+    def handle(self):
+        fd = self.f.fileno()
+        self.sched.waitforread(self.task, fd)
+
+
+class WriteWait(SystemCall):
+    def __init__(self, f) -> None:
+        self.f = f
+
+    def handle(self):
+        fd = self.f.fileno()
+        self.sched.waitforwrite(self.task, fd)
+
+
+class Scheduler:
+    def __init__(self):
+        self.ready = Queue()
+        self.taskmap = {}
+        self.exit_waiting = {}
+        self.read_waiting = {}
+        self.write_waiting = {}
+
+    def new(self, target):
+        newtask = Task(target)
+        self.taskmap[newtask.tid] = newtask
+        self.schedule(newtask)
+        return newtask.tid
+
+    def schedule(self, task):
+        self.ready.put(task)
+
+    def exit(self, task):
+        print("task %s terminated" % task.tid)
+        del self.taskmap[task.tid]
+        for task in self.exit_waiting.pop(task.tid, []):
+            self.schedule(task)
+
+    def waitforexit(self, task, waittid):
+        if waittid in self.taskmap:
+            self.exit_waiting.setdefault(waittid, []).append(task)
+            return True
+        else:
+            return False
+
+    def waitforread(self, task, fd):
+        self.read_waiting[fd] = task
+
+    def waitforwrite(self, task, fd):
+        self.write_waiting[fd] = task
+
+    def iopoll(self, timeout):
+        if self.read_waiting or self.write_waiting:
+            r, w, _ = select.select(self.read_waiting,
+                                    self.write_waiting,
+                                    [],
+                                    timeout)
+            for fd in r:
+                self.schedule(self.read_waiting.pop(fd))
+            for fd in w:
+                self.schedule(self.write_waiting.pop(fd))
+
+    def iotask(self):
+        while True:
+            if self.ready.empty():
+                self.iopoll(None)
+            else:
+                self.iopoll(0)
+            yield
+
+    def mainloop(self):
+        self.new(self.iotask())
+        while self.taskmap:
+            task = self.ready.get()
+            try:
+                result = task.run()
+                if isinstance(result, SystemCall):
+                    result.task = task
+                    result.sched = self
+                    result.handle()
+                    continue
+            except StopIteration:
+                self.exit(task)
+                continue
+            self.schedule(task)
